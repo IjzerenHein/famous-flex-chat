df009ab3c4da6184ad1a3020bca6326d796162c7
IjzerenHein <hrutjes@gmail.com>
2014-10-30 16:19:51 +0100

    Fixed performance issue in AutosizeTextareaSurface due to getting currentTarget.offdetHeight every render-cycle

--- src/AutosizeTextareaSurface.js
+++ src/AutosizeTextareaSurface.js
@@ -26,11 +26,33 @@ define(function(require, exports, module) {
      * @param {Object} [options] Configuration options
      */
     function AutosizeTextareaSurface(options) {
+        this._heightInvalidated = true;
+        this._oldCachedSize = [0, 0];
         _createHiddenSurface.call(this);
         TextareaSurface.apply(this, arguments);
+        this.on('change', _onValueChanged.bind(this));
+        this.on('keyup', _onValueChanged.bind(this));
     }
     AutosizeTextareaSurface.prototype = Object.create(TextareaSurface.prototype);
     AutosizeTextareaSurface.prototype.constructor = AutosizeTextareaSurface;
+
+    //
+    // Called whenever the value is changed, copies the value to the
+    // hidden TextArea surface.
+    //
+    function _onValueChanged(event) {
+        var value = event.currentTarget.value;
+        if (this._hiddenTextarea._currentTarget) {
+            this._hiddenTextarea._currentTarget.value = value;
+        }
+        else {
+            if (value === '') {
+                value = ' ';
+            }
+            this._hiddenTextarea.setValue(value);
+        }
+        this._heightInvalidated = true;
+    }
 
     /**
      * Create the hidden text-area surface
@@ -46,23 +68,50 @@ define(function(require, exports, module) {
      * emits an event about the preferred height.
      */
     AutosizeTextareaSurface.prototype.render = function render() {
-        if (this._currentTarget) {
-            if (this._scrollHeightCache !== this._currentTarget.scrollHeight) {
-                this._scrollHeightCache = this._currentTarget.scrollHeight;
-                this._hiddenTextarea._currentTarget.value = this.getValue();
+
+        // Return render-spec of both this textArea and the hidden
+        // text-area so that they are both rendered.
+        return [this._hiddenTextarea.id, this.id];
+    };
+
+    var oldCommit = AutosizeTextareaSurface.prototype.commit;
+    /**
+     * Apply changes from this component to the corresponding document element.
+     * This includes changes to classes, styles, size, content, opacity, origin,
+     * and matrix transforms.
+     *
+     * @private
+     * @method commit
+     * @param {Context} context commit context
+     */
+    AutosizeTextareaSurface.prototype.commit = function commit(context) {
+
+        // Call base class
+        oldCommit.apply(this, arguments);
+
+        // Check if height has been changed
+        if ((this._oldCachedSize[0] !== context.size[0]) ||
+            (this._oldCachedSize[1] !== context.size[1])) {
+            this._oldCachedSize[0] = context.size[0];
+            this._oldCachedSize[1] = context.size[1];
+            this._heightInvalidated = true;
+        }
+
+        // Caluclate preferred height
+        if (this._hiddenTextarea._currentTarget && this._heightInvalidated) {
+            this._heightInvalidated = false;
+            var scrollHeight = this._hiddenTextarea._currentTarget.scrollHeight;
+            if (this._scrollHeightCache !== scrollHeight) {
+                this._scrollHeightCache = scrollHeight;
                 this._hiddenTextarea._currentTarget.style.height = '10px';
-                var preferredScrollHeight = this._hiddenTextarea._currentTarget.scrollHeight;
-                if (preferredScrollHeight !== this._preferredScrollHeight) {
-                    this._preferredScrollHeight = preferredScrollHeight;
+                scrollHeight = this._hiddenTextarea._currentTarget.scrollHeight;
+                if (scrollHeight !== this._preferredScrollHeight) {
+                    this._preferredScrollHeight = scrollHeight;
                     //console.log('scrollHeight changed: ' + this._preferredScrollHeight);
                     this._eventOutput.emit('scrollHeightChanged', this._preferredScrollHeight);
                 }
             }
         }
-
-        // Return render-spec of both this textArea and the hidden
-        // text-area so that they are both rendered.
-        return [this.id, this._hiddenTextarea.id];
     };
 
     /**
@@ -84,6 +133,7 @@ define(function(require, exports, module) {
         }
         hiddenProperties.visibility = 'hidden';
         this._hiddenTextarea.setProperties(hiddenProperties);
+        this._heightInvalidated = true;
         return oldSetProperties.call(this, properties);
     };
 
@@ -93,56 +143,67 @@ define(function(require, exports, module) {
      */
     var oldSetAttributes = AutosizeTextareaSurface.prototype.setAttributes;
     AutosizeTextareaSurface.prototype.setAttributes = function setAttributes(attributes) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setAttributes(attributes);
         return oldSetAttributes.call(this, attributes);
     };
     var oldAddClass = AutosizeTextareaSurface.prototype.addClass;
     AutosizeTextareaSurface.prototype.addClass = function addClass(className) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.addClass(className);
         return oldAddClass.call(this, className);
     };
     var oldRemoveClass = AutosizeTextareaSurface.prototype.removeClass;
     AutosizeTextareaSurface.prototype.removeClass = function removeClass(className) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.removeClass(className);
         return oldRemoveClass.call(this, className);
     };
     var oldToggleClass = AutosizeTextareaSurface.prototype.toggleClass;
     AutosizeTextareaSurface.prototype.toggleClass = function toggleClass(className) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.toggleClass(className);
         return oldToggleClass.call(this, className);
     };
     var oldSetClasses = AutosizeTextareaSurface.prototype.setClasses;
     AutosizeTextareaSurface.prototype.setClasses = function setClasses(classList) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setClasses(classList);
         return oldSetClasses.call(this, classList);
     };
     var oldSetContent = AutosizeTextareaSurface.prototype.setContent;
     AutosizeTextareaSurface.prototype.setContent = function setContent(content) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setContent(content);
         return oldSetContent.call(this, content);
     };
     var oldSetOptions = AutosizeTextareaSurface.prototype.setOptions;
     AutosizeTextareaSurface.prototype.setOptions = function setOptions(options) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setOptions(options);
         return oldSetOptions.call(this, options);
     };
     var oldSetValue = AutosizeTextareaSurface.prototype.setValue;
     AutosizeTextareaSurface.prototype.setValue = function setValue(str) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setValue(str);
         return oldSetValue.call(this, str);
     };
     var oldSetWrap = AutosizeTextareaSurface.prototype.setWrap;
     AutosizeTextareaSurface.prototype.setWrap = function setWrap(str) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setWrap(str);
         return oldSetWrap.call(this, str);
     };
     var oldSetColumns = AutosizeTextareaSurface.prototype.setColumns;
     AutosizeTextareaSurface.prototype.setColumns = function setColumns(num) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setColumns(num);
         return oldSetColumns.call(this, num);
     };
     var oldSetRows = AutosizeTextareaSurface.prototype.setRows;
     AutosizeTextareaSurface.prototype.setRows = function setRows(num) {
+        this._heightInvalidated = true;
         this._hiddenTextarea.setRows(num);
         return oldSetRows.call(this, num);
     };
